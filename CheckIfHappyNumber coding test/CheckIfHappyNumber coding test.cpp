60b282a2b94a83f5215f26f3aced6ee8d32fee03
Mohammedwich <45858931+Mohammedwich@users.noreply.github.com>
2019-08-09 13:22:02 -0500

    Program written

--- CheckIfHappyNumber coding test/CheckIfHappyNumber coding test.cpp
+++ CheckIfHappyNumber coding test/CheckIfHappyNumber coding test.cpp
@@ -1,11 +1,67 @@
-// CheckIfHappyNumber coding test.cpp : Defines the entry point for the console application.
+// Data structures practice.cpp : Defines the entry point for the console application.
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int checkIfHappy(int theNumber)
+{
+	int digitSquaredSum = 0;
+	int currentNumber = theNumber;
+	static int iterations = 0; //To stop endless recursion
+
+	while (theNumber != 0 && iterations < 25)
+	{
+		int digit = theNumber % 10;
+		theNumber = theNumber / 10;
+		digitSquaredSum += (digit * digit);
+	}
+
+	currentNumber = digitSquaredSum;
+	++iterations;
+
+	if (iterations > 24)
+	{
+		iterations = 0;
+		return 0;
+	}
+	else if (currentNumber > 1)
+	{
+		checkIfHappy(currentNumber);
+	}
+	else if (currentNumber == 1)
+	{
+		iterations = 0;
+		return 1;
+	}
+	else
+	{
+		iterations = 0;
+		return 0;
+	}
+}
 
 
 int main()
 {
-    return 0;
+
+	string line;
+	while (getline(cin, line))
+	{
+		int input = stoi(line);
+		int result = checkIfHappy(input);
+		cout << result << endl;
+	}
+
+	/*
+	std::cout << std::endl << std::endl;
+	int leaveProgram;
+	std::cout << "enter anything to exit: ";
+	std::cin >> leaveProgram;
+	*/
+	return 0;
 }
 
